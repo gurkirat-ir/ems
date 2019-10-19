d814ecb9fb5b9bee05844281ab65348269a24b65
Gurkirat Singh <gurkirat@4iresearch.com>
2019-10-19 10:33:07 +0530

    added 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>404 not found</div>
+  <v-container fill-height style="height: calc(100vh - 58px);">
+    <v-layout align-center>
+      <v-flex text-center>
+        <h1 class="display-2 black--text">Whoops, 404</h1>
+        <br />
+        <p>The page you were looking for does not exist</p>
+        <br />
+        <br />
+        <br />
+        <v-btn :to="{name:'dashboard'}" outlined color="black">Get me out of here!</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "FourOhFour"
+};
 </script>
-
-<style></style>
